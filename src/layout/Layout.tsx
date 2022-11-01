78669874c8fd62ca396bd1447eea2ecb3876b941
Jongco <narira0922@gmail.com>
2022-11-01 22:49:26 +0900

    feature: layout 추가

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode } from "react";
+import styled from "styled-components";
+
+type LayoutProps = {
+  children: ReactNode;
+};
+
+const Layout = ({ children }: LayoutProps) => {
+  return <StyledLayout>{children}</StyledLayout>;
+};
+
+const StyledLayout = styled.div`
+  width: 100%;
+  max-width: 1000px;
+  margin: auto;
+`;
+
+export default Layout;
