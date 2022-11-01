e39f5f776d05e34b0a22700854c7b996fe8dd241
Jongco <narira0922@gmail.com>
2022-11-02 02:40:20 +0900

    feature: detail페이지 연결

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation, useNavigate, useParams, useSearchParams } from "react-router-dom";
+import instance from "../app/instance";
+import { Article } from "../app/states/recoilArticleState";
 
 const Detail = () => {
-  return <div></div>;
+  const [searchParams, _] = useSearchParams();
+  const { mode } = useParams();
+  const id = searchParams.get("id");
+  const [article, setArticle] = useState({} as Article);
+  const navigator = useNavigate();
+
+  useEffect(() => {
+    getDetailArticle();
+  }, []);
+
+  const getDetailArticle = async () => {
+    const { data } = await instance.get(`/${mode}-posts/${id}`);
+    setArticle(data);
+  };
+
+  return (
+    <div>
+      <h1>{article.title}</h1>
+      <p>{article.content}</p>
+      <button
+        onClick={() => {
+          navigator(-1);
+        }}>
+        뒤로가기
+      </button>
+    </div>
+  );
 };
 
 export default Detail;
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import Home from "../pages/Home";
+import Detail from "../pages/Detail";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />}></Route>
+        <Route path='/:mode' element={<Detail />}></Route>
       </Routes>
     </BrowserRouter>
   );
