d589beba6424f8b167a8c60159e637006ee8b45b
Jongco <narira0922@gmail.com>
2022-11-02 14:45:04 +0900

    feature: react-query로 변경

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,28 +3,39 @@ import Navbar from "../components/NavBar";
 import Layout from "../layout/Layout";
 import instance from "../app/instance";
 import ArticleBlock from "../components/ArticleBlock";
-import { useRecoilState } from "recoil";
-import { recoilArticleState, ArticleType } from "../app/states/recoilArticleState";
-import { useQuery } from "react-query";
+import { useQueries, useQuery, useInfiniteQuery } from "react-query";
+
+export type Article = {
+  id: string; // 게시물 ID
+  title: string; // 게시물 제목
+  content: string; // 게시물 내용
+  type: ArticleType; // a or b
+  createdAt: string; // 게시물 생성일 (의미없음)
+};
+
+export type ArticleType = "a" | "b";
 
 const Home = () => {
   const [mode, setMode] = useState<ArticleType>("a");
   const [keyword, setKeyword] = useState("");
-  const [recoilState, setRecoilState] = useRecoilState(recoilArticleState);
-  const page = mode === "a" ? recoilState.a.page : recoilState.b.page;
+  const [page, setPage] = useState({ a: 0, b: 0 });
 
-  useEffect(() => {
-    if (recoilState[mode].articleList.length === 0) getArticle(0);
-  }, [mode]);
-
-  useEffect(() => {
-    getArticle(page);
-  }, [recoilState.a.page, recoilState.b.page]);
-
-  const getArticle = async (page: number) => {
+  const getArticle = async (mode: ArticleType, page: number) => {
     const { data } = await instance.get(`/${mode}-posts?page=${page}`);
-    setRecoilState((prev) => ({ ...prev, [mode]: { ...recoilState[mode], articleList: [...recoilState[mode].articleList, ...data] } }));
+    return { data, nextPage: page + 1 };
   };
+
+  const resultA = useInfiniteQuery("infiniteArticleA", ({ pageParam = 0 }) => getArticle("a", pageParam), {
+    getNextPageParam: (last) => last.nextPage,
+    refetchOnWindowFocus: false,
+  });
+
+  const resultB = useInfiniteQuery("infiniteArticleB", ({ pageParam = 0 }) => getArticle("b", pageParam), {
+    getNextPageParam: (last) => last.nextPage,
+    refetchOnWindowFocus: false,
+  });
+
+  console.log(resultA.data?.pages.map((page) => page.data).flat());
 
   return (
     <Layout>
@@ -37,15 +48,21 @@ const Home = () => {
       />
       <button
         onClick={() => {
-          setRecoilState((prev) => ({ ...prev, [mode]: { ...recoilState[mode], page: recoilState[mode].page + 1 } }));
+          resultA.fetchNextPage();
         }}>
-        page up!
+        page up!A
+      </button>
+      <button
+        onClick={() => {
+          resultB.fetchNextPage();
+        }}>
+        page up!B
       </button>
       <Navbar setMode={setMode} />
       <main>
-        {recoilState[mode].articleList.map((article) => {
+        {/* {data?.map((article) => {
           return <ArticleBlock article={article} key={`${article.type}${article.id}`} />;
-        })}
+        })} */}
       </main>
     </Layout>
   );
