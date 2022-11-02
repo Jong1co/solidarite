820c9415a0fc0914f475707283eca5a05aa17ac9
Jongco <narira0922@gmail.com>
2022-11-03 02:12:12 +0900

    refactor: 데이터가 전부 불려온 후에도 observer때문에 렌더링 발생
    hasNextPage를 통해 존재하지 않으면 observer 제거

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,11 @@
 import { useState, useEffect } from "react";
 import Navbar from "../components/NavBar";
 import Layout from "../layout/Layout";
-import instance from "../app/instance";
 import ArticleBlock from "../components/ArticleBlock";
-import { useQueries, useQuery, useInfiniteQuery, useQueryClient } from "react-query";
 import useInfiniteState from "../hooks/useInfiniteState";
 import { useInView } from "react-intersection-observer";
 import useThrottle from "../hooks/useThrottle";
-
-export type Article = {
-  id: string; // 게시물 ID
-  title: string; // 게시물 제목
-  content: string; // 게시물 내용
-  type: ArticleType; // a or b
-  createdAt: string; // 게시물 생성일 (의미없음)
-};
-
-export type ArticleType = "a" | "b";
+import { Article, ArticleType } from "../typings";
 
 const Home = () => {
   const [mode, setMode] = useState<ArticleType>(sessionStorage.mode || "a");
@@ -25,35 +14,20 @@ const Home = () => {
   const [ref, inView] = useInView();
   const [throttle, setThrottle] = useThrottle(false, 50);
 
-  useEffect(() => {
-    setThrottle();
-  }, []);
-
-  // const queryClient = useQueryClient();
-
-  // useEffect(() => {
-  // queryClient.invalidateQueries();
-  // }, [keyword]);
-
-  /**
-   * 데이터를 두 번씩 요청하는 현상
-   * 첫 화면 렌더링 시 0번 페이지 뿐만 아니라 1번까지 요청하는 현상
-   * => 요청 후 딜레이 필요 => 쓰로틀링으로 하면 될듯
-   */
   const [loadNextA, pageAList, hasNextPageA]: any = useInfiniteState("infiniteArticleA", "a", keyword);
   const [loadNextB, pageBList, hasNextPageB]: any = useInfiniteState("infiniteArticleB", "b", keyword);
 
   const page = mode === "a" ? pageAList : pageBList;
+  const inViewTarget = mode === "a" ? hasNextPageA : hasNextPageB;
 
   if (inView && throttle) {
-    if (mode === "a" && hasNextPageA) {
-      loadNextA();
-      setThrottle();
-    } else if (mode === "b" && hasNextPageB) {
-      loadNextB();
-      setThrottle();
-    }
+    mode === "a" ? hasNextPageA && loadNextA() : hasNextPageB && loadNextB();
+    setThrottle();
   }
+
+  useEffect(() => {
+    setThrottle();
+  }, []);
 
   /** 어떻게 스토리지에 저장 안 하고 b post에 남아있을까? query때문인가 */
   useEffect(() => {
@@ -74,7 +48,7 @@ const Home = () => {
         {page?.map((article: Article) => {
           return <ArticleBlock article={article} key={`${article.type}${article.id}`} />;
         })}
-        <div ref={ref} />
+        {inViewTarget && <div ref={ref} />}
       </main>
     </Layout>
   );
